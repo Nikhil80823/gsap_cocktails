9f1077154f59c4bca18f4fbe1d95248bd1fb64f2
Nikhil Bhadwal <nikhilbhadwal808@gmail.com>
2025-10-07 21:18:47 +0530

    changed the navbar name in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { ScrollTrigger, SplitText } from 'gsap/all'
 import gsap from 'gsap'
-import Navbar from './components/navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Cocktail from './components/Cocktail';
 import About from './components/About';
